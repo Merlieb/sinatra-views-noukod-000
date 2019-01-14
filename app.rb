bd814542c49260274a3d0c0f84ab802a86bdd756
Merline Belizaire <merline@noukod.com>
2019-01-14 17:26:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
+	get '/index' do
 		"<h1>Hello World</h1>"
 	end
 
-	get '/index' do
+	get '/' do
       erb :index
     end
 
